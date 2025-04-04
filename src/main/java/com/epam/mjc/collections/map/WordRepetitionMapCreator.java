853b3d517f9dbe739fd0548fe645c2b81ae4ae6b
Fwalt237 <patrickkamwa17@gmail.com>
2025-04-04 21:48:12 +0100

    update++

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
-        String[] words = sentence.split(" ");
+        String[] words = sentence.split("\\s+");
         Map<String, Integer> wordRepetitionMap = new HashMap<>();
         for (String word : words) {
             if(!wordRepetitionMap.containsKey(word.toLowerCase())) {
                 int counter = 0;
-                for (int j = 0; j < word.length(); j++) {
-                    if (word.equalsIgnoreCase(words[j])) {
+                for (String s : words) {
+                    if (word.equalsIgnoreCase(s)) {
                         counter++;
                     }
                 }
